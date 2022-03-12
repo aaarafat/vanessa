9d9c4212b459e02b453046139b8283a24dcfa60e
aaarafat <aaarafat@yahoo.com>
2022-03-12 21:04:58 +0200

    Communicate through sockets  - Add Flask socket.io  - Client script w/ python socket.io

--- apps/scripts/server.py
+++ apps/scripts/server.py
@@ -1,21 +1,52 @@
 
-import socket
+from multiprocessing import Process
+import threading
+from flask_socketio import SocketIO,emit
+from flask import Flask
 
 HOST = "127.0.0.1"  
 PORT = 65432 
+APP = Flask("mininet")
 
-def server():
-    s = socket.socket()
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, address = s.accept() 
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+
+sio = SocketIO(APP)
+
+@sio.on('connect')
+def connected():
+    print('Connected')
+
+@sio.on('disconnect')
+def disconnected():
+    print('Disconnected')
+
+@sio.on('test')
+def test(message):
+    if(message["data"] == "stop"):
+        stop()
+    print(message["data"])
+    emit('testResponse', {'data': message["data"] + " recieved"})
+
+
+def run():
+    """
+    This function to run configured uvicorn server.
+    """
+    sio.run(APP, host=HOST, port=PORT)
+
+
+def start():
+    """
+    This function to start a new process (start the UDI Publisher server).
+    """
+    threading.Thread(target=run, daemon=True).start()
+
+
+
+
+
 
 if __name__ == '__main__':
-    server()
+    start()
+    print("is it working ?")
+    while True:
+        x = input("->")
