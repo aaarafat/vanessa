448af7bbae2dbf6ab3aae2ad86f7fae214895e70
aaarafat <aaarafat@yahoo.com>
2022-05-27 22:18:39 +0200

    Add Read/Write go script

---
+++ apps/scripts/rw.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	. "github.com/aaarafat/vanessa/apps/network/datalink"
+)
+
+func read(d *DataLinkLayerChannel) {
+	for {
+
+		payload, addr, err := d.Read()
+		if err != nil {
+			log.Fatalf("failed to read from channel: %v", err)
+		}
+		fmt.Println()
+		log.Printf("Received \"%s\" from: [ %s ]", string(payload), addr.String())
+	}
+
+}
+
+func main() {
+
+	d, err := NewDataLinkLayerChannel(VEtherType)
+	if err != nil {
+		log.Fatalf("failed to create channel: %v", err)
+	}
+	go read(d)
+
+	var message string
+	var mtype int
+	for {
+
+		fmt.Scanf("%d %s", &mtype, &message)
+		switch mtype {
+		case 0:
+			d.Broadcast([]byte(message))
+		case 1:
+			log.Print("flooding")
+		}
+	}
+}
