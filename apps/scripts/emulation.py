b6b81ae70c62c64ee66fc39ff74d508dd5515770
fuboki10 <abdelrahman.tarek1910@gmail.com>
2022-07-04 00:52:38 +0200

    add rsu pool to emulation.py

--- apps/scripts/emulation.py
+++ apps/scripts/emulation.py
@@ -39,9 +39,11 @@ LINK_CONFIG = {
 running = True
 
 stations_pool = []
+rsus_pool = []
 stations_car = {}
 
 STATIONS_COUNT = 5
+RSU_COUNT = 5
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
@@ -212,96 +214,40 @@ def to_grid(coordinates):
 info("*** Creating nodes\n")
 
 
-stations["car1"] = net.addStation('car1', position="50, 100, 0",
-                                  **car_kwargs)
-stations["car2"] = net.addStation('car2', position="100, 100, 0",
-                                  **car_kwargs)
-stations["car3"] = net.addStation('car3', position="140, 100, 0",
-                                  **car_kwargs)
-
-
-rsu1 = net.addAccessPoint('rsu1', ssid='VANESSA', mode='g', channel='1',
-                          failMode="standalone", position='15,70,0', range=100,
-                          ip='10.1.0.1/16', cls=UserAP, inNamespace=True)
-rsu2 = net.addAccessPoint('rsu2', ssid='VANESSA', mode='g', channel='1',
-                          failMode="standalone", position='45,70,0', range=100,
-                          ip='10.1.0.2/16', cls=UserAP, inNamespace=True)
-rsu3 = net.addAccessPoint('rsu3', ssid='VANESSA', mode='g', channel='1',
-                          failMode="standalone", position='75,70,0', range=100,
-                          ip='10.1.0.3/16', cls=UserAP, inNamespace=True)
-
 c1 = net.addController('c1')
 s0 = net.addSwitch("s0", cls=UserSwitch, inNamespace=True)
 
 
 def topology(args):
 
+    info("*** Configuring Propagation Model\n")
     net.setPropagationModel(model="logDistance", exp=4)
 
-    info("*** Configuring wifi nodes\n")
+    for i in range(STATIONS_COUNT):
+        stations_pool.append(net.addStation(
+            f'car{i + 1}', position="0,0,0", **car_kwargs))
+
+    for i in range(RSU_COUNT):
+        rsus_pool.append(net.addAccessPoint(f'rsu{i + 1}', ssid='VANESSA', mode='g', channel='1',
+                                            failMode="standalone", position='0,0,0', range=100,
+                                            ip=f'10.1.0.{i + 1}/16', cls=UserAP, inNamespace=True))
+
     net.configureWifiNodes()
 
-    info("*** Creating links\n")
-    # MANET routing protocols supported by proto:
-    # babel, batman_adv, batmand and olsr
-    # WARNING: we may need to stop Network Manager if you want
-    # to work with babel
-    protocols = ['babel', 'batman_adv', 'batmand', 'olsrd', 'olsrd2']
-    kwargs = dict()
+    for i, st in enumerate(stations_pool):
+        net.addLink(st, cls=adhoc,
+                    intf=f'car{i + 1}-wlan0', **LINK_CONFIG)
+        st.setIP(ip=f'10.0.1.{i + 1}/24', intf=f'car{i + 1}-wlan1')
 
-    net.addLink(stations["car1"], cls=adhoc, intf='car1-wlan0',
-                ssid='adhocNet', mode='g', channel=5,
-                ht_cap='HT40+',  **kwargs)
-    stations["car1"].setIP('10.0.1.1/24',
-                           intf='car1-wlan1')
-    net.addLink(stations["car2"], cls=adhoc, intf='car2-wlan0',
-                ssid='adhocNet', mode='g', channel=5,
-                ht_cap='HT40+',  **kwargs)
-    stations["car2"].setIP('10.0.1.2/24',
-                           intf='car2-wlan1')
-    net.addLink(stations["car3"], cls=adhoc, intf='car3-wlan0',
-                ssid='adhocNet', mode='g', channel=5,
-                ht_cap='HT40+', **kwargs)
-    stations["car3"].setIP('10.0.1.3/24',
-                           intf='car3-wlan1')
+    for i, rsu in enumerate(rsus_pool):
+        net.addLink(s0, rsu)
 
-    net.addLink(s0, rsu1)
-    net.addLink(s0, rsu2)
-    net.addLink(s0, rsu3)
-
-    # net.addLink(ap1, ap2)
-    info("*** Plotting network\n")
-    net.plotGraph(max_x=500, max_y=500)
-    # net.setMobilityModel(time=0, model='RandomDirection', max_x=1000, max_y=1000,
-    #                      min_v=10, max_v=100, seed=20)
-
-    # 802.11b standard defines 13 channels on the 2.4 GHz band at 2.4835 Ghz,
-    # allocating 22 MHz for each channel, with a spacing of 5 MHz among them.
-    # With this arrangement, only channels 1, 6 and 11 can operate without band
-    # overlap.
     info("*** Starting network\n")
     net.build()
     c1.start()
     s0.start([c1])
-    rsu1.start([c1])
-    rsu2.start([c1])
-    rsu3.start([c1])
-
-    # stations["car1"].cmd('sysctl net.ipv4.ip_forward=1')
-    # stations["car2"].cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
-    # stations["car3"].cmd('sysctl net.ipv4.ip_forward=1')
-
-    metadata = {'mac': dict(),
-                'mac2ip': dict()}
-    for id in stations:
-        metadata['mac'][stations[id].name] = stations[id].wintfs[0].mac
-        metadata['mac2ip'][stations[id].wintfs[0].mac] = stations[id].wintfs[0].ip
-    save("/tmp/mn.metadata.json", json.dumps(metadata))
-    for id in stations:
-        stations[id].cmd(
-            f'sudo {os.path.join(os.path.dirname(__file__), "../../dist/apps/router")} {stations[id].name} {len(stations)} &')
-    # ap2.cmd(f"./rsuWatcher {ap2.wintfs[0].ssid}")
-    # ap1.cmd(f"./rsuWatcher {ap1.wintfs[0].ssid}")
+    for i, rsu in enumerate(rsus_pool):
+        rsu.start([c1])
 
     info("*** Running CLI\n")
     CLI(net)
