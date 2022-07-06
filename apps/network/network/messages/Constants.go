96a604c2699e8b4e91d537fd9b2e5796b3058c6a
aaarafat <aaarafat@yahoo.com>
2022-07-06 22:39:53 +0200

    Update constants

--- apps/network/network/messages/Constants.go
+++ apps/network/network/messages/Constants.go
@@ -6,7 +6,7 @@ import "net"
 type VHBeatMessage struct {
 	// Type
 	Type uint8  
-	// The IP address of the node that originated the RREQ.
+	// The IP address of the node that originated the Heart Beat.
 	OriginatorIP net.IP
 	// The X cooardinates of the car 
 	PositionX uint32 
@@ -14,12 +14,24 @@ type VHBeatMessage struct {
 	PositionY uint32
 }
 
-
+// Obstcale found message from car to the associated RSU
+type VObstcaleMessage struct {
+	// Type
+	Type uint8  
+	// The IP address of the node that originated the Obstcale alert.
+	OriginatorIP net.IP
+	// The X cooardinates of the obstacle 
+	PositionX uint32 
+	// The Y cooardinates of the obstacle
+	PositionY uint32
+}
 
 const (
 	VHBeatMessageLen = 13
+	VObstcaleMessageLen = 13
 )
 
 const (
 	VHBeatType uint8 = 1
+	VObstcaleType uint8 = 2
 )
