30dab91667c87afa56ddc3b124d8fe4269b5ef64
aaarafat <aaarafat@yahoo.com>
2022-07-07 13:40:05 +0200

    Use Position in Message Types

--- apps/network/network/messages/Constants.go
+++ apps/network/network/messages/Constants.go
@@ -25,10 +25,8 @@ type VHBeatMessage struct {
 	Type uint8  
 	// The IP address of the node that originated the Heart Beat.
 	OriginatorIP net.IP
-	// The X cooardinates of the car 
-	PositionX uint32 
-	// The Y cooardinates of the car
-	PositionY uint32
+	// Position of the node that originated the Heart Beat.
+	Position Position
 }
 
 // Obstcale found message from car to the associated RSU
@@ -37,10 +35,8 @@ type VObstacleMessage struct {
 	Type uint8  
 	// The IP address of the node that originated the Obstcale alert.
 	OriginatorIP net.IP
-	// The X cooardinates of the obstacle 
-	PositionX uint32 
-	// The Y cooardinates of the obstacle
-	PositionY uint32
+    // Position of the obstacle
+	Position Position
 	// Set obstacle to true if the obstacle is detected
 	Clear uint8
 }
@@ -64,8 +60,8 @@ type VOREPMessage struct {
 }
 
 const (
-	VHBeatMessageLen = 13
-	VObstacleMessageLen = 14
+	VHBeatMessageLen = 21
+	VObstacleMessageLen = 22
 	VOREQMessageLen = 5
 )
 
