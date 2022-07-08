c16fc385b33c50cdfa33992ccb418b02df034eba
Hassan Mohamed <hmibrahim1999@gmail.com>
2022-07-08 15:31:22 -0400

    edit position json

--- apps/network/network/messages/Constants.go
+++ apps/network/network/messages/Constants.go
@@ -3,14 +3,15 @@ package messages
 import "net"
 
 type Position struct {
-	Lat float64
-	Lng float64
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
 }
 
 // Postion to byte array marashalling
 func (p Position) Marshal() []byte {
 	return append(Float64bytes(p.Lat), Float64bytes(p.Lng)...)
 }
+
 // Position from byte array unmarshalling
 func UnmarshalPosition(data []byte) Position {
 	return Position{
@@ -22,7 +23,7 @@ func UnmarshalPosition(data []byte) Position {
 // Heart beat message from car to the associated RSU
 type VHBeatMessage struct {
 	// Type
-	Type uint8  
+	Type uint8
 	// The IP address of the node that originated the Heart Beat.
 	OriginatorIP net.IP
 	// Position of the node that originated the Heart Beat.
@@ -32,10 +33,10 @@ type VHBeatMessage struct {
 // Obstcale found message from car to the associated RSU
 type VObstacleMessage struct {
 	// Type
-	Type uint8  
+	Type uint8
 	// The IP address of the node that originated the Obstcale alert.
 	OriginatorIP net.IP
-    // Position of the obstacle
+	// Position of the obstacle
 	Position Position
 	// Set obstacle to true if the obstacle is detected
 	Clear uint8
@@ -43,31 +44,29 @@ type VObstacleMessage struct {
 
 type VOREQMessage struct {
 	// Type
-	Type uint8  
+	Type uint8
 	// The IP address of the node that originated the Obstcales request.
 	OriginatorIP net.IP
-
 }
 
 type VOREPMessage struct {
 	// Type
-	Type uint8  
+	Type uint8
 	// length of the list of Obstcales
 	Length uint8
 	// content of the list of Obstcales
 	Obstcales []byte
-
 }
 
 const (
-	VHBeatMessageLen = 21
+	VHBeatMessageLen    = 21
 	VObstacleMessageLen = 22
-	VOREQMessageLen = 5
+	VOREQMessageLen     = 5
 )
 
 const (
-	VHBeatType uint8 = 1
+	VHBeatType    uint8 = 1
 	VObstacleType uint8 = 2
-	VOREQType uint8 = 3
-	VOREPType uint8 = 4
+	VOREQType     uint8 = 3
+	VOREPType     uint8 = 4
 )
