8483e71aac5d57f8380fa730a9726f38baab0421
fuboki10 <abdelrahman.tarek1910@gmail.com>
2022-07-10 17:31:52 +0200

    Add logs in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+echo "Building Car Application....."
 go build -o dist/apps/car apps/car/main.go
+echo "Building Network....."
 go build -o dist/apps/network apps/network/main.go
+echo "Building Switch....."
 go build -o apps/scripts/switch apps/scripts/switch.go
+echo "Done Building!!!!"
