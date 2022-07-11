21829c3a33bf6770f1f092b3302fd0cb697f1e18
aaarafat <aaarafat@yahoo.com>
2022-07-11 18:00:34 +0200

    Add ARP

--- apps/rsu-ui/src/app/interface.tsx
+++ apps/rsu-ui/src/app/interface.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ARP } from './ARP';
 import { ObstaclesTable } from './obstacles-table';
 
+const STables = styled.div`
+  display: flex;
+`;
+const OTable = styled.div`
+  width: 30%;
+`;
+const ARPTable = styled.div`
+  width: 70%;
+`;
 export const Interface: React.FC = () => {
   return (
-    <div>
-      RSU interface
-      <ObstaclesTable />
-    </div>
+    <STables>
+      <OTable>
+        <ObstaclesTable />
+      </OTable>
+      <ARPTable>
+        <ARP />
+      </ARPTable>
+    </STables>
   );
 };
