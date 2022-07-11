90189f9c0d4dd883958ee726b21245ceb294fff7
aaarafat <aaarafat@yahoo.com>
2022-07-11 19:06:45 +0200

    Update interface

--- apps/rsu-ui/src/app/interface.tsx
+++ apps/rsu-ui/src/app/interface.tsx
@@ -17,23 +17,39 @@ const SCards = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
+const SHeading = styled.h1`
+  font-size: 5rem;
+  font-family: 'Bebas Neue', cursive;
+  margin: 1rem;
+  color: #0d0d0d;
+`;
+const SDiscription = styled.h3`
+  font-size: 2.5rem;
+  font-family: 'Lato', sans-serif;
+  margin: 1rem;
+  color: #ffc000;
+`;
 export const Interface: React.FC = () => {
   return (
     <>
+      <SHeading>RSU Overview</SHeading>
+      <SDiscription>Total packets</SDiscription>
       <SCards>
         <InfoCard
           title="Sent Packets"
           info="9999999"
           color="#ffffff"
-          bgc="#ffc000"
+          bgc="#0d0d0d"
         />
         <InfoCard
           title="Received Packets"
           info="9999999"
           color="#ffffff"
-          bgc="#0d0d0d"
+          bgc="#3d3d3d"
         />
       </SCards>
+      <SDiscription>Current State</SDiscription>
+
       <STables>
         <OTable>
           <ObstaclesTable />
