628e0ccc53f802df2105c61e17ca627e1cb68978
fuboki10 <abdelrahman.tarek1910@gmail.com>
2022-07-15 18:53:54 +0200

    Add install mnwifi script

---
+++ apps/scripts/install-mnwifi.sh
@@ -0,0 +1,6 @@
+echo "Clone mnwifi repository...."
+git clone git://github.com/intrig-unicamp/mininet-wifi
+echo "Clone mnwifi repository....done"
+echo "Install mnwifi repository...."
+sudo mininet-wifi/util/install.sh -Wlnfv
+echo "Install mnwifi repository....done"
